7d95971516851cd235a540d8a7cbec017177bcd8
Pogrebock <vikapogrebniak16@gmail.com>
2020-07-03 20:32:56 +0300

    Разработка тестового проекта Карусель

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/Header/Header";
+import Slide from "./components/Slide/Slide";
+import DB from "./DB.json";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import picture_1 from "./img/picture_1.jpg"
+import picture_2 from "./img/picture_2.jpg"
+import picture_3 from "./img/picture_3.jpg"
+import picture_4 from "./img/picture_4.jpg"
+import picture_5 from "./img/picture_5.jpg"
+import picture_6 from "./img/picture_6.jpg"
+import picture_7 from "./img/picture_7.jpg"
+import picture_8 from "./img/picture_8.jpg"
+import picture_9 from "./img/picture_9.jpg"
+import picture_10 from "./img/picture_10.jpg"
+import left_svg from "./img/arrows/left.svg"
+import right_svg from "./img/arrows/right.svg"
+
 function App() {
+   
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+
+  try{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Carousel">
+      <Header />
+      <Slider {...settings}>
+        {DB.slides.map((slide, index) =>
+          <Slide
+            key={index}
+            src={slide.picture}
+            content={slide.content}
+          />
+        )}
+      </Slider>
     </div>
-  );
+  )
+} catch {}
 }
 
+   
 export default App;
