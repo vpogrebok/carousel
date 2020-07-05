3b8259027b40942b9aac13e50e17207bffddb63a
Pogrebock <vikapogrebniak16@gmail.com>
2020-07-05 14:24:03 +0300

    Доработки карусели

--- src/App.js
+++ src/App.js
@@ -6,18 +6,6 @@ import Slide from "./components/Slide/Slide";
 import DB from "./DB.json";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import picture_1 from "./img/picture_1.jpg"
-import picture_2 from "./img/picture_2.jpg"
-import picture_3 from "./img/picture_3.jpg"
-import picture_4 from "./img/picture_4.jpg"
-import picture_5 from "./img/picture_5.jpg"
-import picture_6 from "./img/picture_6.jpg"
-import picture_7 from "./img/picture_7.jpg"
-import picture_8 from "./img/picture_8.jpg"
-import picture_9 from "./img/picture_9.jpg"
-import picture_10 from "./img/picture_10.jpg"
-import left_svg from "./img/arrows/left.svg"
-import right_svg from "./img/arrows/right.svg"
 
 function App() {
    
@@ -28,7 +16,6 @@ function App() {
     slidesToScroll: 1
   }
 
-  try{
   return (
     <div className="Carousel">
       <Header />
@@ -43,8 +30,6 @@ function App() {
       </Slider>
     </div>
   )
-} catch {}
 }
 
-   
 export default App;
--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -4,11 +4,12 @@ import "./Slide.css"
 const Slide = ({src, content}) => {
     const [height, setHeight] = useState("22px");
     const [overflow, setOverflow] = useState("hidden");
+    const [title, setTitle] = useState("Читать далee")
 
     const changeVisibilityText = () => {
         content.length > 100 && height === "22px"
-            ? setOverflow("visible") || setHeight("100%")
-            : setOverflow("hidden") || setHeight("22px")
+            ? setOverflow("visible") || setHeight("100%") || setTitle("Скрыть")
+            : setOverflow("hidden") || setHeight("22px") || setTitle("Читать далee")
     }
     
     return(
@@ -17,7 +18,7 @@ const Slide = ({src, content}) => {
             <img className="slide__img" src={src} alt="Изображение природы" />
             <p className="slide__content" style={{height, overflow}}>{content}</p>
             </div>
-            <button className="slide__button" type="button" onClick={changeVisibilityText}>Читать далee</button>
+            <button className="slide__button" type="button" onClick={changeVisibilityText}>{title}</button>
         </div>
     )
 }
